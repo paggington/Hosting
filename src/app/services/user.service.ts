23752c0e53c56a1463ef92e47ee2ebeaaa5b5bd2
Artem <lobach.artem.v@gmail.com>
2022-03-24 23:46:39 +0100

    user service API

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,21 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {User} from "../models/User.model";
 import {Observable} from "rxjs";
+import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
-
-  constructor(private http:HttpClient) { }
-
-  getUserDataByUsername(username:string):Observable<User>{
+export class UserService{
+  user!:User;
+  constructor(private http:HttpClient) {
+    this.getUserDataByUsername().subscribe(user=>this.user=user);
+  }
+  getUserDataByUsername():Observable<User>{
     let options={
-      params:new HttpParams().set('username',username),
       // @ts-ignore
       headers:new HttpHeaders().set('Authorization','Bearer '+localStorage.getItem('a_token'))
     }
-    return this.http.get<User>("http://localhost:8080/api/v1/users/user",options)
+    return this.http.get<User>("http://localhost:8080/api/v1/users/current-user",options)
   }
+  getUserProfileImage():Observable<Blob>{
+    let storedUser=localStorage.getItem("currentUser");
+    // @ts-ignore
+    this.user=JSON.parse(storedUser);
+    let headers=new HttpHeaders().set('Authorization','Bearer '+localStorage.getItem('a_token'))
+    return this.http.get<Blob>("http://localhost:8080/api/v1/users/profile-picture",
+      {headers,responseType:'blob' as 'json'})
+  }
+
 }
