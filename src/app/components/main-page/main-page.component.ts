0965fb2d3f4aea8e9e930fb0ec9e5b4a72c88766
Artem <lobach.artem.v@gmail.com>
2022-03-24 23:45:49 +0100

    main videos

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Video} from "../../models/Video.model";
+import {VideoService} from "../../services/video.service";
+import {SnackErrorAuthService} from "../../services/errors/snack-error-auth.service";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-
-  constructor() { }
+  videos:Video[]=[];
+  private page=0;
+  private totalPages=0;
+  private size=6;
+  constructor(private videoService:VideoService,
+              private snack:SnackErrorAuthService) { }
 
   ngOnInit(): void {
+    this.getVideos();
   }
-
+  getVideos(){
+    this.videoService.getVideosForNotAuthenticated(this.page,this.size).subscribe(vid=>{
+      // @ts-ignore
+      for (const video of vid.content) {
+        this.videos.push(video)
+      }
+    },error => {
+      if(error.status==500||503){
+        this.snack.errorVideo("Server error, try again later")
+      }
+    });
+  }
 }
