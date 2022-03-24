f4c8f64d4a01c3c85f1d8f89aeb29342de7ce0a4
Artem <lobach.artem.v@gmail.com>
2022-03-24 23:46:58 +0100

    videos service API

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {Video} from "../models/Video.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideoService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+              private router: Router) {
+  }
+
+  getVideosForNotAuthenticated(page: number, size: number): Observable<Video[]> {
+    let params = {
+      params: new HttpParams()
+        .set('page', page)
+        .set('size', size)
+    }
+    return this.http.get<Video[]>("http://localhost:8080/api/v1/video/get-all-new", params);
+  }
+
+  getVideosForAuthenticated(): Observable<Video[]> | null {
+    let options = {
+      headers: new HttpHeaders()
+        .set('Authorization', 'Bearer ' + localStorage.getItem('a_token'))
+    }
+    return null;
+  }
+  getVideoPreview(id:string):Observable<Blob>{
+    let params={
+      params:new HttpParams()
+        .set("id",id),
+      headers: new HttpHeaders()
+        .set('Content-Type','application/octet-stream'),
+      responseType:'blob' as 'json'
+    }
+    return this.http.get<Blob>("http://localhost:8080/api/v1/video/preview",params)
+  }
 }
