1b472ef9846cc85ac76e118c293427aa7db73a6d
Artem <lobach.artem.v@gmail.com>
2022-03-24 23:46:19 +0100

    main app navbar

--- src/app/components/shell/shell.component.ts
+++ src/app/components/shell/shell.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {User} from "../../models/User.model";
 
 @Component({
   selector: 'app-shell',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shell.component.css']
 })
 export class ShellComponent implements OnInit {
-  isSidenavOpened=false;
-  constructor() { }
+  user!:User;
+  imageProfile:Blob=new Blob();
+  imageBlobUrl: string | ArrayBuffer | null ="";
+  constructor(private userService:UserService,private jwtHelper:JwtHelperService) { }
 
   ngOnInit(): void {
+    // @ts-ignore
+    this.userService.getUserDataByUsername(this.jwtHelper.decodeToken(localStorage.getItem('a_token')).getSubject)
+    this.getProfileImage();
+    this.user=this.userService.user;
   }
-
+  getProfileImage(){
+    this.userService.getUserProfileImage().subscribe(data=>{
+      this.imageProfile=data;
+      this.createImageFromBlob()
+    })
+  }
+  createImageFromBlob(){
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+      this.imageBlobUrl = reader.result;
+    }, false);
+    if (this.imageProfile) {
+      return reader.readAsDataURL(this.imageProfile);
+    }
+  }
 }
