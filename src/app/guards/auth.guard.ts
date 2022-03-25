6a4d9af561aea37747f67619fa6cd20a0fe9b819
Artem <lobach.artem.v@gmail.com>
2022-03-25 14:13:59 +0100

    Auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,37 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import {AuthService} from "../services/auth.service";
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {SnackErrorAuthService} from "../services/errors/snack-error-auth.service";
+import {ShellComponent} from "../components/shell/shell.component";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
+  constructor(private authService:AuthService,private jwtHelper:JwtHelperService,private snack:SnackErrorAuthService) {
+  }
+  async canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): Promise<boolean>{
+    const user=this.authService.getCurrentUser();
+    if(localStorage.getItem('a_token')&&localStorage.getItem('r_token')==null||undefined){
+      return false;
+      //@ts-ignore
+    }else if(this.jwtHelper.isTokenExpired(localStorage.getItem('a_token'))){
+      this.authService.refreshTokens().subscribe(tokens=>{
+        localStorage.setItem('r_token',tokens.refresh_token);
+        localStorage.setItem('a_token',tokens.access_token);
+      },error => {
+        if(error.status==403||401){
+          localStorage.clear();
+          this.snack.errorTokens();
+        }
+      })
+      return true;
+    }
     return true;
   }
-  
+
 }
