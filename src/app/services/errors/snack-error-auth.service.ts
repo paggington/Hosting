f26d1b30309b4c4ec88f9cc78e7298e814db5b9e
Artem <lobach.artem.v@gmail.com>
2022-03-25 14:14:48 +0100

    error popups

--- src/app/services/errors/snack-error-auth.service.ts
+++ src/app/services/errors/snack-error-auth.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {tap} from "rxjs";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackErrorAuthService {
-  constructor(private snack:MatSnackBar) { }
+  constructor(private snack:MatSnackBar,private router:Router) { }
 
   errorLogin(){
     this.snack.open('Try again!','X',{
       duration:5000,
       horizontalPosition:'start'
     })
+  }
+  errorTokens(){
+    this.snack.open('You need to login to watch your subs','OK',{
+      duration:5000,
+      horizontalPosition:'start'
+    })
+    return this.snack._openedSnackBarRef?.onAction().pipe(
+      tap(_=>{
+        this.router.navigateByUrl('/login')
+      })
+    ).subscribe();
   }
   successLogin(message:string){
     this.snack.open(message,'X',{
